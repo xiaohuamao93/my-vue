4ceb8fe085a8d75bf939dc748c6c4abed7f63a6c
Xiaohua Mao <xiaohua.mao@uuloop.com>
2018-08-08 17:13:59 +0800

    Add drag

---
+++ src/components/drag.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
+    <vue-draggable-resizable :w="100" :h="100" @dragging="onDrag" @resizing="onResize" :parent="true">
+      <p>X: {{ x }} / Y: {{ y }} - Width: {{ width }} / Height: {{ height }}</p>
+    </vue-draggable-resizable>
+  </div>
+</template>
+
+<script>
+//import VueDraggableResizable from 'vue-draggable-resizable'
+export default {
+  name: 'Drag',
+  data () {
+    return {
+      width: 0,
+      height: 0,
+      x: 0,
+      y: 0
+    }
+  },
+  methods: {
+    onResize: function (x, y, width, height) {
+      this.x = x
+      this.y = y
+      this.width = width
+      this.height = height
+    },
+    onDrag: function (x, y) {
+      this.x = x
+      this.y = y
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .draggable, .resizable {
+      border: 1px solid #333;
+      user-select: none;
+    }
+    .active {
+      cursor: all-scroll;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Drag from '@/components/drag'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/drag',
+      name: 'Drag',
+      component: Drag
     }
   ]
 })
